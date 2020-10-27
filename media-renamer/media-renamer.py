5852b6196da255f84886590d4ce06eb1c198a870
Krzysztof Herok <kherok@novomatic-tech.com>
2020-10-27 18:38:58 +0100

    Generate a new media file name and rename the existing file. Create a bash script that installs required python libraries.

--- media-renamer/media-renamer.py
+++ media-renamer/media-renamer.py
@@ -6,25 +6,42 @@ import os
 import argparse
 from termcolor import colored
 
-
+DATE_TIME_ORIGINAL_TAG = 36867
 valid_photo_extensions = [".jpg", ".jpeg"]
 valid_video_extensions = [".mp4", ".mov"]
 
 
 def read_photo_original_datetime(photo_path):
-    date_time_original = Image.open(photo_path)._getexif()[36867]
-    print("DateTime Original: " + date_time_original)
-    date_time_original_obj = datetime.strptime(date_time_original, "%Y:%m:%d %H:%M:%S")
-    return date_time_original_obj.strftime("%Y-%m-%d_%H:%M:%S")
+    date_time_original = Image.open(photo_path)._getexif()[DATE_TIME_ORIGINAL_TAG]
+    return datetime.strptime(date_time_original, "%Y:%m:%d %H:%M:%S")
 
 
 def read_video_encoded_datetime(video_path):
     media_info = MediaInfo.parse(video_path)
     for track in media_info.tracks:
         if track.track_type == 'General':
-            print(track.encoded_date)
             encoded_date = datetime.strptime(track.encoded_date, "UTC %Y-%m-%d %H:%M:%S").replace(tzinfo=pytz.UTC)
-            return encoded_date.astimezone().strftime("%Y-%m-%d_%H:%M:%S")
+            return encoded_date.astimezone()
+
+
+def rename_file(original_path, new_path):
+    print("{} -> {}".format(colored(original_path, "yellow"), colored(new_path, "green")))
+    os.rename(original_path, new_path)
+
+
+def create_unique_file_name(output_dir_path, formatted_date, file_ext, file_name_postfix):
+    file_name_candidate = formatted_date
+    if file_name_postfix > 0:
+        file_name_candidate += "-{}".format(file_name_postfix)
+    if os.path.exists(os.path.join(output_dir_path, file_name_candidate + file_ext)):
+        file_name_postfix += 1
+        return create_unique_file_name(output_dir_path, formatted_date, file_ext, file_name_postfix)
+    return file_name_candidate
+
+
+def get_new_file_name(output_dir_path, media_creation_date, file_ext):
+    formatted_date = media_creation_date.strftime("%Y-%m-%d_%H:%M:%S")
+    return create_unique_file_name(output_dir_path, formatted_date, file_ext, 0)
 
 
 def parse_args():
@@ -45,11 +62,13 @@ def main():
         for file in files:
             file_ext = os.path.splitext(file)[1]
             if file_ext.lower() in valid_photo_extensions:
-                print("Image: {}".format(os.path.join(root, file)))
-                print("New name: {}\n".format(read_photo_original_datetime(os.path.join(root, file))))
+                creation_date = read_photo_original_datetime(os.path.join(root, file))
+                new_file_name = get_new_file_name(root, creation_date, file_ext)
+                rename_file(os.path.join(root, file), os.path.join(root, new_file_name + file_ext))
             elif file_ext.lower() in valid_video_extensions:
-                print("Video: {}".format(os.path.join(root, file)))
-                print("New name: {}\n".format(read_video_encoded_datetime(os.path.join(root, file))))
+                creation_date = read_video_encoded_datetime(os.path.join(root, file))
+                new_file_name = get_new_file_name(root, creation_date, file_ext)
+                rename_file(os.path.join(root, file), os.path.join(root, new_file_name + file_ext))
 
 
 if __name__ == '__main__':
---
+++ media-renamer/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install --upgrade Pillow
+pip3 install --upgrade pymediainfo
+pip3 install --upgrade pytz
+pip3 install --upgrade termcolor
